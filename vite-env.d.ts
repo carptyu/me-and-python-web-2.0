4f233c62e378da749c1371007f58f5e34a248054
carptyu <carptyu@gmail.com>
2025-11-20 17:28:47 +0800

    Add files via upload

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,5 +1,7 @@
 interface ImportMetaEnv {
   readonly VITE_API_KEY: string;
+  readonly VITE_CONTENTFUL_SPACE_ID: string;
+  readonly VITE_CONTENTFUL_ACCESS_TOKEN: string;
   // more env variables...
 }
 
